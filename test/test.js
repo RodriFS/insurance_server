f185c6870e953c5578f05926221033ffdf2e1f36
Rodrigo Sanchez <rodrifs@gmail.com>
2018-11-26 17:29:57 +0100

    test: add signin testing

--- test/test.js
+++ test/test.js
@@ -37,6 +37,37 @@ describe('loading express and endpoints', () => {
         .set('Accept', 'application/json')
         .expect(400);
     });
+
+    describe('Endpoint /signin', () => {
+      it('400 if data is missing', () => {
+        request(server)
+          .post('/signin')
+          .send({ name: 'john' })
+          .set('Accept', 'application/json')
+          .expect(400);
+      });
+      it('400 if user is not found', () => {
+        request(server)
+          .post('/signin')
+          .send({ name: 'notauser', password: 'password' })
+          .set('Accept', 'application/json')
+          .expect(400);
+      });
+      it('400 if password is invalid', () => {
+        request(server)
+          .post('/signin')
+          .send({ name: 'john', password: 'incorrectpassword' })
+          .set('Accept', 'application/json')
+          .expect(400);
+      });
+      it('200 if user and password is correct', () => {
+        request(server)
+          .post('/signin')
+          .send({ name: 'john', password: 'helloworld' })
+          .set('Accept', 'application/json')
+          .expect(200);
+      });
+    });
     it('404 everything else', () => {
       request(server)
         .get('/foo/bar')
